08d0cf510f62b02b51800ee13eff6262d33d899f
Max <jmschwartz8@gmail.com>
2024-05-22 08:52:39 -0400

    added marketing deployment

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -16,7 +16,7 @@ const prodConfig = {
 			name: "container",
 			remotes: [
 				{
-					marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+					marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
 				},
 			],
 			shared: packageJson.dependencies,
